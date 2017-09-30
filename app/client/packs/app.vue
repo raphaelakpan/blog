72d6cb28d1ea91c137ffdd82606aa28a0d7ba095
Raphael Ifiok Akpan <raphael.akpan@andela.com>
2017-09-30 05:46:21 +0100

    Add vuejs

---
+++ app/client/packs/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <input type="text" placeholder="Type here..." v-model="message"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
